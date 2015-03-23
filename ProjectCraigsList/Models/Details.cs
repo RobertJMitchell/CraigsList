b7080717c8d8d55583eb67055e2ece9f51e2a1e3
RobertJMitchell <rjrocks32@gmail.com>
2015-03-23 14:31:16 -0500

    make new imagesize viewmodel

---
+++ ProjectCraigsList/Models/Details.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectCraigsList.Models
+{
+    public abstract class Details
+    {
+        public string Imagesize { get; set; }
+    }
+}
--- ProjectCraigsList/Models/ItemHolder.cs
+++ ProjectCraigsList/Models/ItemHolder.cs
@@ -15,6 +15,7 @@ namespace ProjectCraigsList.Models
             new Item() { ID = 101, Description = "3/2/2 Two Story Family Home", Price=1300.00, CreatedBy = "Sue Smith", CreatedOnDate = DateTime.Now.AddDays(-4), City = "Sugar Land", State = "Texas" },
             new Item() { ID = 102, Description = "MUST SEE 5/4/2 MCMANSION", Price=2500.00, CreatedBy = "Tyrone Biggums", CreatedOnDate = DateTime.Now.AddDays(-1), City = "Galveston", State = "Texas" },
             new Item() { ID = 103, Description = "1/1 Shack, Low Overhead", Price=500.00, CreatedBy = "The Hood", CreatedOnDate = DateTime.Now.AddDays(-2), City = "Houston", State = "Texas" },
+            new Item() {ID = }
         };
             return iList;
       }
